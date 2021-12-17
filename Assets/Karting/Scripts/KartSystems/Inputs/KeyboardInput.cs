13081a96b53a26cd37a4a9fdd98f7388e26fd91f
modyabdo391 <83193905+modyabdo391@users.noreply.github.com>
2021-12-17 22:01:21 +0200

    added controls

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+
+    public class KeyboardInput : BaseInput
+    {
+        private GameFlowManager flowManager;
+        void Start() => flowManager = FindObjectOfType<GameFlowManager>();
+        public FixedJoystick movejoystick;
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+        private float hoz;
+        void Update()
+        {
+            hoz = movejoystick.Horizontal * .7f;
+        }
+        public override InputData GenerateInput()
+        {
+            return new InputData
+            {
+
+                Accelerate = flowManager.acceleration == 1 ? true : false,
+                Brake = flowManager.braking == -1 ? true : false,
+                TurnInput = hoz
+
+            };
+        }
+    }
+}
