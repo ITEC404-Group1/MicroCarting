d9302f8534ab6d82e8154aacaffb48392277b0c6
modyabdo391 <83193905+modyabdo391@users.noreply.github.com>
2021-12-27 22:18:32 +0200

    update_position of player

--- Assets/Karting/Scripts/winner_loser/CarController.cs
+++ Assets/Karting/Scripts/winner_loser/CarController.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public bool isAI;
-    public int currentTarget;
-    private Vector3 targetPoint;
-    public float aiAccelerateSpeed = 1f, aiTurnSpeed = .8f, aiReachRange = 5f, aiPointVariance = 3f;
     private float aiSpeedInput;
-    private int nextCheckpoint;
+    public int nextCheckpoint;
     public int currentLap;
     public float lapTime, bestLapTime;
     public static CarController instance;
@@ -19,11 +16,7 @@ public class CarController : MonoBehaviour
     }
     void Start()
     {
-        if (isAI)
-        {
-            targetPoint = race_manger.ins.allcheckpoints[currentTarget].transform.position;
-            RandomisAITarget();
-        }
+     
     }
     void Update()
     {
@@ -32,21 +25,7 @@ public class CarController : MonoBehaviour
 
         var ts = System.TimeSpan.FromSeconds(lapTime);
         UImanager.instance.currentLapText.text = string.Format("{0:00}m{1:00}.{2:000}s", ts.Minutes, ts.Seconds, ts.Milliseconds);
-        if (isAI)
-        {
-            targetPoint.y = transform.position.y;
-            if (Vector3.Distance(transform.position, targetPoint) < aiReachRange)
-            {
-                currentTarget++;
-                if (currentTarget >= race_manger.ins.allcheckpoints.Length)
-                {
-                    currentTarget = 0;
-                }
-                targetPoint = race_manger.ins.allcheckpoints[currentTarget].transform.position;
-                RandomisAITarget();
-
-            }
-        }
+        UImanager.instance.postionPlayer.text = race_manger.ins.playerPosition + "/" + 3;
         
     }
     public void CheckpointHit(int cpNumber)
@@ -78,16 +57,13 @@ public class CarController : MonoBehaviour
             UImanager.instance.bestLapTimeText.text = string.Format("{0:00}m{1:00}.{2:000}s", ts.Minutes, ts.Seconds, ts.Milliseconds);
             if (race_manger.ins.totalLaps>=currentLap)
             {
-            UImanager.instance.lapCounterText.text = currentLap + "/" + race_manger.ins.totalLaps; 
+            UImanager.instance.lapCounterText.text = currentLap + "/" + race_manger.ins.totalLaps;
+              
             }
             }
             
 
         }
         lapTime = 0f;
-    }
-    public void RandomisAITarget()
-    {
-        targetPoint += new Vector3(Random.Range(-aiPointVariance, aiPointVariance), 0f, Random.Range(-aiPointVariance, aiPointVariance));
     }
 }
--- Assets/Karting/Scripts/winner_loser/race_manger.cs
+++ Assets/Karting/Scripts/winner_loser/race_manger.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class race_manger : MonoBehaviour
 {
+    public CarController playerCar;
+    public List<CarController> allAICars = new List<CarController>();
     public static race_manger ins;
+    public int playerPosition;
+    public float timeBetweenPosCheck = .2f;
+    private float posChkCounter;
     public check_point[] allcheckpoints;
     public int totalLaps;
     private void Awake()
@@ -22,6 +27,32 @@ public class race_manger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        posChkCounter -= Time.deltaTime;
+        if (posChkCounter <= 0)
+        {
+            playerPosition = 1;
+            foreach (CarController aiCar in allAICars)
+            {
+                if (aiCar.currentLap > playerCar.currentLap)
+                {
+                    playerPosition++;
+                }else if (aiCar.currentLap == playerCar.currentLap)
+                {
+                    if (aiCar.nextCheckpoint > playerCar.nextCheckpoint)
+                    {
+                        playerPosition++;
+                    }
+                    else if (aiCar.nextCheckpoint == playerCar.nextCheckpoint)
+                    {
+                        if (Vector3.Distance(aiCar.transform.position, allcheckpoints[aiCar.nextCheckpoint].transform.position) < Vector3.Distance(aiCar.transform.position, allcheckpoints[aiCar.nextCheckpoint].transform.position))
+                        {
+                            playerPosition++;
+                        }
+                    }
+                }
+            }
+            posChkCounter = timeBetweenPosCheck;
+        }
 
     }
 }
