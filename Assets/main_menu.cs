ca38ea49d185c10b87f9544ca57b5515c45a4a00
Dovud <dinomov99@gmail.com>
2022-01-03 12:59:20 +0200

    modify the format of the stats page and display dynamically based on the database values

--- Assets/main_menu.cs
+++ Assets/main_menu.cs
@@ -2,41 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Model;
 
 public class main_menu : MonoBehaviour
 {
-    public GameObject helpScreen,statisticScreen,startScreen;
-    public TMP_Text totalRacing,totalWins,totalScore;
-    public List<TMP_Text> trackNo = new List<TMP_Text>();
-    public List<TMP_Text> positionNo = new List<TMP_Text>();
+    public GameObject helpScreen, statisticScreen, startScreen;
+    public TMP_Text totalRacing, totalWins, totalScore;
+    public List<TMP_Text> MapName = new List<TMP_Text>();
+    public List<TMP_Text> Position = new List<TMP_Text>();
+    public List<TMP_Text> Won = new List<TMP_Text>();
+    public List<TMP_Text> XP = new List<TMP_Text>();
+
+    public Model.FirebaseManager FBManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (FBManager.UserId == "")
+        {
+            FBManager.UserId = "NEKEOhAnJNUf96EfSkZCOHwphuJ3";
+        }
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        var i = 1;
-        foreach(TMP_Text track in trackNo)
-        {
-            
-            track.text=""+i;
-            i++;
-        }
-        i = 10;
-        foreach(TMP_Text position in positionNo)
-        {
-            position.text=""+i;
-            i++;
-        }
-        totalRacing.text="10";
-        totalWins.text="8";
-        totalScore.text="250";
-        
-    }
+    void Update() { }
     public void openHelp()
     {
         helpScreen.SetActive(true);
@@ -48,6 +38,22 @@ public class main_menu : MonoBehaviour
     public void openStatistic()
     {
         statisticScreen.SetActive(true);
+
+        var scores = FBManager.GetUserScores();
+        int limit = 3,  i = 0;
+        foreach (Model.ScoreElement score in scores)
+        {
+            if (i == limit)
+                break;
+
+            Debug.Log(score.MapName);
+            MapName[i].text = "" + score.MapName;
+            Position[i].text = "" + score.Position;
+            Won[i].text = "" + score.Won;
+            XP[i].text = "" + score.XP;
+
+            i++;
+        }
     }
     public void closeStatistic()
     {
